be25571d7873a2246fe0814540992bd87651a1e3
Taha Emre Yavuz <56791446+teyavuz@users.noreply.github.com>
2024-06-09 00:29:32 +0300

    Continuous spawn

--- Assets/Scripts/SonicScrewdriver.cs
+++ Assets/Scripts/SonicScrewdriver.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class SonicScrewdriver : MonoBehaviour
 {
     [SerializeField] private ParticleSystem pSys;
-    private void OnTriggerEnter(Collider other) 
+    [SerializeField] private GameObject[] spawnpoints;
+    [SerializeField] private GameObject Angel;
+
+
+    private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Angel")
         {
             Debug.Log("Melek öldü");
             Destroy(other.gameObject);
             pSys.Play();
+
+            int randomSpawnPointNumber;
+
+            randomSpawnPointNumber = UnityEngine.Random.Range(0, spawnpoints.Length);
+
+            Instantiate(Angel, spawnpoints[randomSpawnPointNumber].transform.position, spawnpoints[randomSpawnPointNumber].transform.rotation);
         }
     }
 }
